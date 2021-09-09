17dae302fb63a3025858f1f83e7e78ac6c1d363d
Riki Ramadhani <rikiramadhani08@gmail.com>
2021-09-10 06:34:15 +0700

    plugin hapi and validation

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
 
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.46.156',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
@@ -12,7 +16,13 @@ const init = async () => {
     },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
